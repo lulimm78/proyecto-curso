e9c8c0348d0b571878d9a200acde909d787e411f
Louordes Méndez <lulimendem@gmail.com>
2024-12-22 15:31:49 +0100

    segundo intento

--- webepresarial/webepresarial/settings.py
+++ webepresarial/webepresarial/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aff1-gxqyiqnqsolr%edu-p8a7oai)8u&yu-_+l^ux*7()5pi3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['manucontreras.pythonanywhere.com','localhots','127.0.0.1','manu-contreras.com']
 
 
 # Application definition
